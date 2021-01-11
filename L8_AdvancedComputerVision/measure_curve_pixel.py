f042254aed31c633c29156d22cb4174c4660d763
Simon Geiger <simongeiger01@gmail.com>
2021-01-12 02:30:37 +0900

    finished lesson 8 with measuring the curve radius

---
+++ L8_AdvancedComputerVision/measure_curve_pixel.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+def generate_data():
+    '''
+    Generates fake data to use for calculating lane curvature.
+    In your own project, you'll ignore this function and instead
+    feed in the output of your lane detection algorithm to
+    the lane curvature calculation.
+    '''
+    # Set random seed number so results are consistent for grader
+    # Comment this out if you'd like to see results on different random data!
+    np.random.seed(0)
+    # Generate some fake data to represent lane-line pixels
+    ploty = np.linspace(0, 719, num=720)# to cover same y-range as image
+    quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
+    # For each y position generate random x position within +/-50 pix
+    # of the line base position in each case (x=200 for left, and x=900 for right)
+    leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
+                                    for y in ploty])
+    rightx = np.array([900 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
+                                    for y in ploty])
+
+    leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
+    rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+
+
+    # Fit a second order polynomial to pixel positions in each fake lane line
+    left_fit = np.polyfit(ploty, leftx, 2)
+    right_fit = np.polyfit(ploty, rightx, 2)
+    
+    return ploty, left_fit, right_fit
+
+def calc_curve_radius(poly_2nd_order, y_eval):
+  return np.abs(1/(2*poly_2nd_order[0])) * np.power(1+(2*poly_2nd_order[0]*y_eval+poly_2nd_order[1])**2,1.5)
+
+    
+def measure_curvature_pixels():
+    '''
+    Calculates the curvature of polynomial functions in pixels.
+    '''
+    # Start by generating our fake example data
+    # Make sure to feed in your real data instead in your project!
+    ploty, left_fit, right_fit = generate_data()
+    
+    # Define y-value where we want radius of curvature
+    # We'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = np.max(ploty)
+    
+    # Implement the calculation of R_curve (radius of curvature)
+    left_curverad = calc_curve_radius(left_fit,y_eval)
+    right_curverad = calc_curve_radius(right_fit,y_eval)
+    
+    return left_curverad, right_curverad
+
+
+# Calculate the radius of curvature in pixels for both lane lines
+left_curverad, right_curverad = measure_curvature_pixels()
+
+print(left_curverad, right_curverad)
+# Should see values of 1625.06 and 1976.30 here, if using
+# the default `generate_data` function with given seed number
